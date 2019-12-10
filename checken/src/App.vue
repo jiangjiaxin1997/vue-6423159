3310a9c7dc76d2494b45664b2a716b9b2bf52bbb
jiangjiaxin1997 <54538427+jiangjiaxin1997@users.noreply.github.com>
2019-12-10 15:11:36 +0800

    2

--- checken/src/App.vue
+++ checken/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-  
+    <Header></Header>
   </div>
 </template>
 <script>
-
+  import Header from './components/Header'
+ export default {
+    components:{Header}
+ }
 </script>
 <style lang="less">/style>
--- checken/src/router/index.js
+++ checken/src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
+import Recommend from '../pages/recommend'
+import Singer from '../pages/singer.vue'
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/recommend',
+    name: 'R1',
+    component: Recommend
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+   path:'/singer',
+   name:'S1',
+   component:Singer
   }
 ]
 
